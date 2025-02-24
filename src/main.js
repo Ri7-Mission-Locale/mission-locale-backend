000d267ebbfb683d7d5a78e6ec7bcb84b87c90c3
kevstfnl <50336969+kevstfnl@users.noreply.github.com>
2025-02-24 11:59:45 +0100

    init project

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import express from "express"
+import cors from "cors"
+
+const port = process.env.NODE_PORT || 3000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, (err) => {
+    if (!err) return console.error(err);
+    console.log(`Listen at port ${port}`)
+})
