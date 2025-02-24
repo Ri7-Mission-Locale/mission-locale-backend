f6a96963025c304eb5275baed42452356af379f2
kevstfnl <50336969+kevstfnl@users.noreply.github.com>
2025-02-24 12:06:31 +0100

    Add  authentication router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,13 @@
 import express from "express"
 import cors from "cors"
+import authenticationRouter from "./routes/authenticationRouter";
 
 const port = process.env.NODE_PORT || 3000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(authenticationRouter);
 
 app.listen(port, (err) => {
     if (!err) return console.error(err);
