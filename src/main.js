9fc8276905fb4b00e7809ae52c12ea46bd10b537
kevstfnl <50336969+kevstfnl@users.noreply.github.com>
2025-02-25 13:29:20 +0100

    Patch conflict

--- src/main.js
+++ src/main.js
@@ -1,21 +1,8 @@
 import express from "express"
 import cors from "cors"
-
-
 import workshopRouter from "./routes/workshopRouter.js";
-
 import meetingRouter from "./routes/meetingRouter.js";
-
-
 import counsellorRouter from "./routes/counsellorRouter.js";
-
-
-
-import counsellorRouter from "./routes/counsellorRouter.js";
-import workshopRouter from "./routes/workshopRouter.js";
-import tagRouter from "./routes/tagRouter.js";
-import meetingRouter from "./routes/meetingRouter.js";
-
 import memberRouter from "./routes/memberRouter.js";
 
 
@@ -24,27 +11,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-
-
-
-
 app.use(counsellorRouter);
-
-
 app.use(workshopRouter)
-
 app.use(meetingRouter)
-
-
-
 app.use(counsellorRouter);
- 
-
-
-
 app.use(memberRouter);
-
 
 app.listen(port, (err) => {
     if (err) return console.error(err);
--- src/validators/memberValidator.js
+++ src/validators/memberValidator.js
@@ -4,8 +4,8 @@ const registerMemberSchema = object({
     firstName: string().min(2).max(50).required(),
     lastName: string().min(2).max(50).required(),
     mail: string().email().required(),
-    password: yup.string().min(8).max(100).required(),
-    repeatPassword: yup.string().min(8).max(100).required(),
+    password: string().min(8).max(100).required(),
+    repeatPassword: string().min(8).max(100).required(),
 })
 
 export default registerMemberSchema;
