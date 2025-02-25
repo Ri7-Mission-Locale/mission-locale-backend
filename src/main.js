2d5ef83693722bcfcbf053b42aab6189c31355dd
Fabien Costabello <costabellofabien@gmail.com>
2025-02-25 11:51:19 +0100

    add employee route

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import express from "express"
 import cors from "cors"
 import authenticationRouter from './routes/authenticationRouter.js'
+import counsellorRouter from "./routes/counsellorRouter.js";
+
 
 const port = process.env.NODE_PORT || 3000;
 const app = express();
@@ -8,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(authenticationRouter);
+app.use(counsellorRouter);
 
 app.listen(port, (err) => {
     if (err) return console.error(err);
--- src/routes/authenticationRouter.js
+++ src/routes/authenticationRouter.js
@@ -4,27 +4,22 @@ const authenticationRouter = express.Router();
 
 /* User's account creation */
 authenticationRouter.put("/auth/register", (req, res) => {
-
 })
 
 /* Update user's mail before validation */
 authenticationRouter.patch("/auth/changemail/:token", (req, res) => {
-
 })
 
 /* User's mail validation */
 authenticationRouter.patch("/auth/validate/:token", (req, res) => {
-
 })
 
 /* Handle user's connexion and store session */
 authenticationRouter.post("/auth/login", (req, res) => {
-
 })
 
 /* Refresh user session  */
 authenticationRouter.get("/auth/refresh", (req, res) => {
-
 })
 
 export default authenticationRouter;
