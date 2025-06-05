a8d9ab099e29c7c26bc481573ae0cb1f6225bd4b
kevstfnl <seventimepro@gmail.com>
2025-06-05 10:07:56 +0200

    minor fix

--- src/databases/database.js
+++ src/databases/database.js
@@ -1,5 +1,6 @@
 import { hashPassword } from "../middlewares/hashPassword.js";
 import { PrismaClient } from "./generated/client.js";
 
+/** @type {PrismaClient} for autocompletion */
 const database = new PrismaClient().$extends(hashPassword);
 export default database;
--- src/middlewares/hashPassword.js
+++ src/middlewares/hashPassword.js
@@ -1,19 +1,17 @@
-import { Prisma } from "@prisma/client";
-import { hashSync }  from "bcrypt";
+import {Prisma} from "@prisma/client";
+import {hashSync} from "bcrypt";
 
 export const hashPassword = Prisma.defineExtension({
     name: "hash",
     query: {
         user: {
             create: async ({ args, query }) => {
-                const hash = hashSync(args.data.password, 10);
-                args.data.password = hash;
+                args.data.password = hashSync(args.data.password, 10);
                 return query(args);
             },
             update: async ({ args, query }) => {
-                if (!args.data.password) return query(args);
-                const hash = hashSync(args.data.password, 10);
-                args.data.password = hash;
+                if (!args.data.password || typeof args.data.password !== 'string') return query(args);
+                args.data.password = hashSync(args.data.password, 10);
                 return query(args);
             },
         }
